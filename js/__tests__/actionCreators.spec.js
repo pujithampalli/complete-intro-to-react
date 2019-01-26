84b57c3d49809b2a8697c09669b8bac448f21966
Venkat kotu <vkotu@paypal.com>
2019-01-26 10:33:00 -0800

    testing async thunks

--- js/__tests__/actionCreators.spec.js
+++ js/__tests__/actionCreators.spec.js
@@ -1,21 +1,44 @@
-import { setSearchTerm, addAPIData } from "../actionCreators";
+// @flow
+import moxios from "moxios";
+import { setSearchTerm, addAPIData, getAPIDetails } from "../actionCreators";
 
 test("setSearchTerm", () => {
   expect(setSearchTerm("black")).toMatchSnapshot();
 });
-
+const blackMirror = {
+  tt2085059: {
+    rating: "0.0",
+    title: "Black Mirror",
+    year: "2011–",
+    description: "A television anthology series that shows the dark side of life and technology.",
+    poster: "bm.jpg",
+    imdbID: "tt2085059",
+    trailer: "jDiYGjp5iFg"
+  }
+};
 test("addAPIData", () => {
-  expect(
-    addAPIData({
-      tt2085059: {
-        rating: "0.0",
-        title: "Black Mirror",
-        year: "2011–",
-        description: "A television anthology series that shows the dark side of life and technology.",
-        poster: "bm.jpg",
-        imdbID: "tt2085059",
-        trailer: "jDiYGjp5iFg"
-      }
-    })
-  ).toMatchSnapshot();
+  expect(addAPIData(blackMirror)).toMatchSnapshot();
+});
+
+test("getAPIDetails", (done: Function) => {
+  const dispatchMock = jest.fn();
+  moxios.withMock(() => {
+    const thunk = getAPIDetails(blackMirror.imdbID);
+    thunk(dispatchMock);
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request
+        .respondWith({
+          status: 200,
+          response: blackMirror
+        })
+        .then(() => {
+          expect(request.url).toEqual(
+            `http://localhost:3000/${blackMirror.imdbID}`
+          );
+          expect(dispatchMock).toBeCalledWith(addAPIData(blackMirror));
+          done();
+        });
+    });
+  });
 });
